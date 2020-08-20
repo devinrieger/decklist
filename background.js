8e89896982db4581ebbc96f982c5979ef1ae0d56
Devin Rieger <devin.rieger@bouncex.com>
2020-08-20 18:10:08 -0400

    whitespace

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ chrome.runtime.onInstalled.addListener(function() {
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	  chrome.tabs.sendMessage(tabs[0].id, {update: 'list'});
+		chrome.tabs.sendMessage(tabs[0].id, {update: 'list'});
 	});
 });
 
