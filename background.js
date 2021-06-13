5e5ab3c7c1d6accf3765836ca5c1460391d54c51
Devin Rieger <devin@alloy.co>
2021-06-13 00:15:32 -0400

    update snytax

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.local.set({list: []});
+	chrome.storage.local.set({ list: [] });
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {update: 'list'});
+		chrome.tabs.sendMessage(tabs[0].id, { update: 'list' });
 	});
 });
 
@@ -23,4 +23,4 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 	}
 });
 
-chrome.browserAction.setBadgeBackgroundColor({color: '#333'});
+chrome.browserAction.setBadgeBackgroundColor({ color: '#333' });
